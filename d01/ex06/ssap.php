a6e5eee15521b571524c2c852f723564fb1fc6cb
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-02 17:55:59 +0200

    ex06: tri et affichage de tous les mots de tous les parametres

--- d01/ex06/ssap.php
+++ d01/ex06/ssap.php
@@ -3,9 +3,7 @@
     foreach ($argv as $value ) {
         if ($value != $argv[0]) {
             $tab = explode(' ', $value);
-            //$tab = array_filter($tab);
             foreach ($tab as $value) {
-                //echo("$value\n");
                 $tab2[] = $value;
             }
         }
