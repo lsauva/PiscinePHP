c27efd57f561caaade378a15ecefde116fc638dd
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-02 17:57:11 +0200

    ex07: 1er mot du parametre en derniere position

---
+++ d01/ex07/rostring.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+    if ($argc > 1) {
+        $tab = explode(' ', $argv[1]);
+        $tab = array_filter($tab);
+        $str = $tab[0];
+        array_push($tab, $str);
+        unset($tab[0]);
+        $tab = array_filter($tab);
+        $str = implode(' ', $tab);
+        echo $str."\n";
+    }
+?>
