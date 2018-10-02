dd18255d5e965d0bbbbc25dfa229f32487f4258b
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-02 15:33:40 +0200

    ft split & sort words from a string

---
+++ d01/ex03/ft_split.php
@@ -0,0 +1,8 @@
+<?php
+    function ft_split($str) {
+        $tab = explode(' ', $str);
+        $tab = array_filter($tab);
+        sort($tab);
+        return($tab);
+    }
+?>
---
+++ d01/ex03/main.php
@@ -0,0 +1,7 @@
+#!/usr/bin/php
+<?php
+# fichier de test pour la fonction ft_split
+include("ft_split.php");
+
+print_r(ft_split("Hello  32  28   World AAA         //..    @  "));
+?>
