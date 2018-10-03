deb56c61877463880e0a8c945f065e4cc21998dd
Laurent Sauvage <lsauvage@student.42.fr>
2018-10-03 03:31:02 +0200

    ex11: do_op_2 + gestion division et modulo par 0

---
+++ d01/ex11/do_op_2.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+if (isset($argv) && isset($argv[1])) {
+    if (preg_match("/^ *(-?[0-9]+) *([\+\-\*\/\%]) *(-?[0-9]+) *$/", $argv[1], $match))
+    {
+        $nbr1 = (int)trim($match[1]);
+        $op = trim($match[2]);
+        $nbr2 = (int)trim($match[3]);
+        
+        if ($op == "+")
+            echo ($nbr1 + $nbr2);
+        else if  ($op == "-")
+            echo ($nbr1 - $nbr2);
+        else if  ($op == "*")
+            echo ($nbr1 * $nbr2);
+        else if  ($op == "/" && $nbr2 == 0)
+            echo ("Syntax Error : Division by zero");
+        else if  ($op == "/")
+            echo ($nbr1 / $nbr2);
+        else if  ($op == "%" && $nbr2 == 0)
+            echo ("Syntax Error : Modulo by zero");
+        else if  ($op == "%")
+            echo ($nbr1 % $nbr2);
+        else 
+            echo ("Syntax Error");
+    }
+    else
+		echo ("Syntax Error");
+}
+else
+	echo ("Incorrect Parameters");
+echo ("\n");
+?>
