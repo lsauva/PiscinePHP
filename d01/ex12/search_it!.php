6d56510847da45e3e7c1d09ec91cbbdfb017a84f
Laurent Sauvage <lsauvage@student.42.fr>
2018-10-03 03:39:35 +0200

    ex12: search it !

---
+++ d01/ex12/search_it!.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+if (isset($argv) && isset($argv[1])) {
+	array_shift($argv);
+	$search = array_shift($argv);
+	foreach ($argv as $arg) {
+		list($index, $value) = explode(':', $arg);
+		if ($index == $search)
+		{
+			echo ($value);
+			break;
+		}
+	}
+}
+echo ("\n");
+?>
