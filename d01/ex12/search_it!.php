6a84fed39b65a39a59d7c0bc10658710f51a09b0
Laurent Sauvage <lsauvage@student.42.fr>
2018-10-03 03:48:25 +0200

    ex12: search it !

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -1,7 +1,10 @@
 #!/usr/bin/php
 <?php
 if (isset($argv) && isset($argv[1])) {
-	array_shift($argv);
+    # array_shift permet de depiler un element du tableau 
+    # ICI le 1er elem du tab argv est son nom
+    array_shift($argv);
+    # search est la clef recherchee (1er param)
 	$search = array_shift($argv);
 	foreach ($argv as $arg) {
 		list($index, $value) = explode(':', $arg);
