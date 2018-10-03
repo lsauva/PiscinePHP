f682a20db10ee4801b50c93db6e1535ed24b83c2
Laurent Sauvage <lsauvage@student.42.fr>
2018-10-03 02:25:47 +0200

    ex06: ssap + gestion 0 param

--- d01/ex06/ssap.php
+++ d01/ex06/ssap.php
@@ -1,15 +1,27 @@
 #!/usr/bin/php
 <?php
-    foreach ($argv as $value ) {
-        if ($value != $argv[0]) {
-            $tab = explode(' ', $value);
-            foreach ($tab as $value) {
-                $tab2[] = $value;
-            }
+# pour chaque parametre du prog
+foreach ($argv as $value ) {
+    # sauf le nom du prog lui-meme
+    if ($value != $argv[0]) {
+        # on retire tous les espaces
+        $tab = explode(' ', $value);
+        # chaque valeur de tab est ajoutee au tableau tab2
+        foreach ($tab as $value) {
+            $tab2[] = $value;
         }
     }
+}
+# si tab2 existe (verif 0 param !)
+if ($tab2) {
+    # on trie le tableau
     sort($tab2);
+    # chaque vakeur de tab2 est affichee sur la sortie
     foreach ($tab2 as $value) {
         echo("$value\n");
     }
+}
+# si on appelle ssap.php sans param
+else
+    echo("\r");
 ?>
