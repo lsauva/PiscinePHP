b20afaa26270bdfd3a32993d2e6958cddb4355c0
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-04 21:46:46 +0200

    connect login pass http

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,17 +1,20 @@
 <?php
-$user = $_SERVER['PHP_AUTH_USER'];
-$password = $_SERVER['PHP_AUTH_PW'];
-
-if ($user.":".$password === "zaz:jaimelespetitsponeys") {
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+    $user = $_SERVER['PHP_AUTH_USER'];
+}
+if (isset($_SERVER['PHP_AUTH_PW'])) {
+    $password = $_SERVER['PHP_AUTH_PW'];
+}
+if (($user && $password) && ($user.":".$password === "zaz:jaimelespetitsponeys")) {
     echo "<html><body>\nBonjour Zaz<br />\n";
     echo "<img src='data:image/png;base64,".base64_encode(file_get_contents("../img/42.png"))."'>\n";
     echo "</body></html>\n";
 }
 else {
-    header(' HTTP/1.0 401 Unauthorized');
-    header(" WWW-Authenticate: Basic realm=''Espace membres''");
-    header('Connection: close');
-    header('Content-Type: text/html');
+    header("HTTP/1.0 401 Unauthorized");
+    header("WWW-Authenticate: Basic realm=''Espace membres''");
+    header("Connection: close");
+    header("Content-Type: text/html");
     echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
 }
 ?>
