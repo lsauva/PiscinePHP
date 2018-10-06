1749f538f995ba640baac774506d21661a96da1c
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-06 22:27:51 +0200

    ex03 auth

--- d04/ex03/auth.php
+++ d04/ex03/auth.php
@@ -1,13 +1,15 @@
 <?php
 function auth($login, $passwd) {
-    # on recupere le contenu du fichier passwd a partir d'une valeur linearisee
-    $tab = unserialize(file_get_contents("../private/passwd"));
-    # pour chaque element cle => valeur du tab
-    foreach ($tab as $key => $elem)
-    {
-        # on teste si les champs dans le fichier et ceux saisi sont correspondants
-        if ($elem['passwd'] == hash('whirlpool', $passwd) && $elem['login'] == $login)
-            return TRUE;
+    if (file_exists("../private/passwd")) {
+        # on recupere le contenu du fichier passwd a partir d'une valeur linearisee
+        $tab = unserialize(file_get_contents("../private/passwd"));
+        # pour chaque element cle => valeur du tab
+        foreach ($tab as $key => $elem)
+        {
+            # on teste si les champs dans le fichier et ceux saisi sont correspondants
+            if ($elem['passwd'] == hash('whirlpool', $passwd) && $elem['login'] == $login)
+                return TRUE;
+        }
     }
     return FALSE;
 }
---
+++ d04/ex03/login.php
@@ -0,0 +1,23 @@
+<?php
+include 'auth.php';
+
+session_start();
+
+if (isset($_GET['login']) && isset($_GET['passwd']))
+{
+	if (auth($_GET['login'], $_GET['passwd']) == true)
+	{
+		$_SESSION['loggued_on_user'] = $_GET['login'];
+		echo "OK\n";
+	}
+	else
+	{
+		$_SESSION['loggued_on_user'] = "";
+		echo "ERROR\n";
+	}
+}
+else
+{
+	echo "ERROR\n";
+}
+?>
