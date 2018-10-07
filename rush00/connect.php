8d7bc7abee89a4986d9ef806bd019763b2e3ab1f
Laurent Sauvage <lsauvage@student.42.fr>
2018-10-07 05:14:20 +0200

    connexion

--- rush00/connect.php
+++ rush00/connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn = db_connect();
-$conn = mysqli_connect("localhost","root","root42","miniboutique") or die("Error " . mysqli_error($link));
+# $conn = db_connect();
+$conn = mysqli_connect("localhost","root","root42","rush00") or die("Error " . mysqli_error($link));
 ?>
