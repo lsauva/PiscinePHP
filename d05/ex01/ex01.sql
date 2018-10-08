b340cc551b55a5dfba9e2e467ab18251e0890f1b
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-08 14:36:35 +0200

    protection champs

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,7 +1,7 @@
 CREATE TABLE db_lsauvage.ft_table (
-    id INT(10) AUTO_INCREMENT NOT NULL,
-    login VARCHAR(8) DEFAULT "toto" NOT NULL,
+    `id` INT(10) AUTO_INCREMENT NOT NULL,
+    `login` VARCHAR(8) DEFAULT "toto" NOT NULL,
     `group` ENUM ('staff', 'student', 'other') NOT NULL,
-    creation_date DATE NOT NULL,
+    `creation_date` DATE NOT NULL,
     PRIMARY KEY (id)
 );
