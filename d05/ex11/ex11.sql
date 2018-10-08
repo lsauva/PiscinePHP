3a7f5beb07b84f932881ac5df627c96e505dad58
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-08 18:14:23 +0200

    INNER JOIN imbrique

---
+++ d05/ex11/ex11.sql
@@ -0,0 +1,31 @@
+-- ### Premiere version sans INNER JOIN
+-- SELECT
+--     UPPER(last_name) NAME,
+--     first_name,
+--     price
+-- FROM
+--     user_card,
+--     member,
+--     subscription
+-- WHERE
+--     user_card.id_user = member.id_user_card
+-- AND member.id_sub = subscription.id_sub
+-- AND subscription.price > 42
+-- ORDER BY
+--     last_name ASC,
+--     first_name ASC;
+
+SELECT
+    UPPER(last_name) NAME,
+    first_name,
+    subscription.price
+FROM
+    ((user_card
+INNER JOIN member ON
+    user_card.id_user = member.id_user_card)
+INNER JOIN subscription ON
+    member.id_sub = subscription.id_sub)
+WHERE subscription.price > 42
+ORDER BY
+    last_name ASC,
+    first_name ASC;
