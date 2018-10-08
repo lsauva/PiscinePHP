e051c74a188ddc5c46d361c0cbb0e179f23417b3
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-08 22:32:14 +0200

    count date between Noel

---
+++ d05/ex16/ex16.sql
@@ -0,0 +1,12 @@
+-- Affichez le nombre total de films visionnés du 30/10/2006 au 27/07/2007
+-- dans une colonne ’films’ en comptant également le nombre de films vus les soirs de Noël
+-- (24 décembre de chaque année).
+
+SELECT
+    COUNT(`date`) AS 'films'
+FROM
+    `member_history`
+WHERE
+    (`date` BETWEEN '2006-10-30' AND '2007-07-27')
+OR
+    (DAY(`date`) = 24 AND MONTH(`date`) = 12);
