5d3a9238c9c6f2e75a57efffb4c048ead16b9fb7
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-09 01:03:43 +0200

    changement jointure : user_card.id_user = member.id_member & member.id_sub = subscription.id_sub

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -22,7 +22,7 @@ SELECT
 FROM
     ((user_card
 INNER JOIN member ON
-    user_card.id_user = member.id_user_card)
+    user_card.id_user = member.id_member)
 INNER JOIN subscription ON
     member.id_sub = subscription.id_sub)
 WHERE subscription.price > 42
