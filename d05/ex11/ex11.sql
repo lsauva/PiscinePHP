939eea5e65a8987ce631dff595cea500071996aa
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-09 12:39:33 +0200

    add protections

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -15,17 +15,37 @@
 --     last_name ASC,
 --     first_name ASC;
 
+-- Version 2 avec INNER JOIN sur `user_card`.`id_user` = `member`.`id_member`
+-- 17 reponses
+
+-- SELECT
+--     UPPER(`user_card`.`last_name`) AS NAME,
+--     `user_card`.`first_name`,
+--     `subscription`.`price`
+-- FROM
+--     `user_card`
+-- INNER JOIN `member` ON
+--     `user_card`.`id_user` = `member`.`id_member`
+-- INNER JOIN `subscription` ON
+--     `member`.`id_sub` = `subscription`.`id_sub`
+-- WHERE `subscription`.`price` > 42
+-- ORDER BY
+--     `last_name` ASC,
+--     `first_name` ASC;
+
+-- version finale, logique au niveau de la construction de la base, pas au niveau des resultats (2 reponses)
+
 SELECT
-    UPPER(last_name) AS NAME,
-    first_name,
-    subscription.price
+    UPPER(`user_card`.`last_name`) AS NAME,
+    `user_card`.`first_name`,
+    `subscription`.`price`
 FROM
-    ((user_card
-INNER JOIN member ON
-    user_card.id_user = member.id_member)
-INNER JOIN subscription ON
-    member.id_sub = subscription.id_sub)
-WHERE subscription.price > 42
+    `user_card`
+INNER JOIN `member` ON
+    `user_card`.`id_user` = `member`.`id_user_card`
+INNER JOIN `subscription` ON
+    `member`.`id_sub` = `subscription`.`id_sub`
+WHERE `subscription`.`price` > 42
 ORDER BY
-    last_name ASC,
-    first_name ASC;
+    `last_name` ASC,
+    `first_name` ASC;
