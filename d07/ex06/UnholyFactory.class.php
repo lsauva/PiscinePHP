bcf424d5137a99e5e759c1716757319e0801d58c
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-10-11 00:17:05 +0200

    private attributes, instanceof, clone, The wrong kind of pact...

--- d07/ex06/UnholyFactory.class.php
+++ d07/ex06/UnholyFactory.class.php
@@ -7,28 +7,28 @@ class UnholyFactory {
     private function _setEnrolledFighters($p) { array_push($this->_enrolled_fighters, $p); return; }
 
     private function _isAlreadyEnrolled($p) {
-        if (in_array($p, $this->_getEnrolledFighters()))
+        if ( in_array( $p, $this->_getEnrolledFighters() ) )
             return True;
         else
             return False;
     }
 
     public function absorb($p) {
-        if ( $this instanceof Fighter) {
-            if ( $this->_isAlreadyEnrolled($p))
-                print('(Factory already absorbed a fighter of type $p)' . PHP_EOL);
+        if ( $p instanceof Fighter ) {
+            if ( $this->_isAlreadyEnrolled($p) )
+                print( "(Factory already absorbed a fighter of type $p)" . PHP_EOL );
             else {
                 $this->_setEnrolledFighters($p);
-                print('(Factory absorbed a fighter of type $p)' . PHP_EOL);
+                print( "(Factory absorbed a fighter of type $p)" . PHP_EOL );
             }
         }
         else
-            print("(Factory can't absorb this, it's not a fighter)" . PHP_EOL );
+            print( "(Factory can't absorb this, it's not a fighter)" . PHP_EOL );
     }
 
     public function fabricate($p) {
-        foreach ( $this->_getEnrolledFighters() as $fighter) {
-            if ($fighter->getFighterType() === $p) {
+        foreach ( $this->_getEnrolledFighters() as $fighter ) {
+            if ( $fighter->getFighterType() === $p ) {
                 print( "(Factory fabricates a fighter of type $p)" . PHP_EOL );
                 return clone $fighter;
             }
